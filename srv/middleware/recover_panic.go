e5fd9a99ac98b0f4c98c0f884202fd577b3656e1
mreleftheros <eleftheros_all@protonmail.com>
2025-07-26 17:10:29 +0300

    add recover panic middleware

---
+++ srv/middleware/recover_panic.go
@@ -0,0 +1,19 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/mreleftheros/gotools/srv/json"
+)
+
+func RecoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				w.Header().Set("Connection", "close")
+				json.WriteInternalError(w, r, err.(error))
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
+}
